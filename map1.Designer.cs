36c4faf431cd94100c3ffc1b84822175273508c5
santi-nue <68065589+santi-nue@users.noreply.github.com>
2025-10-09 09:23:02 +0200

    Update map1.Designer.cs

--- map1.Designer.cs
+++ map1.Designer.cs
@@ -38,7 +38,7 @@
             this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
             this.gMapControl1.GrayScaleMode = false;
             this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
-            this.gMapControl1.LevelsKeepInMemmory = 5;
+           
             this.gMapControl1.Location = new System.Drawing.Point(0, 0);
             this.gMapControl1.MarkersEnabled = true;
             this.gMapControl1.MaxZoom = 18;
@@ -66,7 +66,7 @@
             this.Controls.Add(this.gMapControl1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            
             this.ResumeLayout(false);
         }
 
