b6663143bb55867eda40382f8a09dc2881135220
santi-nue <68065589+santi-nue@users.noreply.github.com>
2025-10-09 09:29:05 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+// Add this to a file, e.g. Program.cs
+using System;
+using System.Windows.Forms;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new Form1());
+    }
+}
