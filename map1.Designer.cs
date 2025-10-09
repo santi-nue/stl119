6dd64ffcaed1623ed63d52500e04c457b5428efe
santi-nue <68065589+santi-nue@users.noreply.github.com>
2025-10-09 09:25:28 +0200

    Update map1.Designer.cs

--- map1.Designer.cs
+++ map1.Designer.cs
@@ -56,7 +56,7 @@
             this.gMapControl1.Size = new System.Drawing.Size(800, 450);
             this.gMapControl1.TabIndex = 0;
             this.gMapControl1.Zoom = 5D;
-            this.gMapControl1.Load += new System.EventHandler(this.gMapControl1_Load);
+          
             // 
             // Form1
             // 
